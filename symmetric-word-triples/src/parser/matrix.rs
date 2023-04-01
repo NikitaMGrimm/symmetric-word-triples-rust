a71059f48308523b30cd91e336eb5653ce17c601
Nikita Maximilian Grimm <nikitagrimm03@gmail.com>
2023-04-02 00:36:46 +0200

    Token

--- symmetric-word-triples/src/parser/matrix.rs
+++ symmetric-word-triples/src/parser/matrix.rs
@@ -1,3 +1,87 @@
+use minivec::MiniVec;
+
+use super::token::Token;
+
+#[derive(Hash, Clone, Debug, PartialEq, Eq)]
+
+pub struct TknMatrix {
+    matrix: MiniVec<Token>,
+    size: usize,
+}
+
+impl TknMatrix {
+    /// Creates a matrix of Tokens of size `tkns` x `tkns`.
+    pub fn new(tkns: usize) -> TknMatrix {
+        let matrix_flat: MiniVec<Token> = MiniVec::with_capacity(tkns * tkns);
+        TknMatrix {
+            matrix: matrix_flat,
+            size: tkns,
+        }
+    }
+
+    // Insert a Token into the matrix at the specified row and column.
+    // Panics if the row or column index is out of bounds of the matrix.
+    #[inline]
+    pub fn insert(&mut self, row: usize, col: usize, tkn: Token) {
+        let flat_i = self.to_flat_index(row, col).unwrap();
+        self.matrix.insert(flat_i, tkn);
+    }
+
+    /// Returns the Token in the specified row and column.
+    /// Panics if the row or column index is out of bounds of the matrix.
+    #[inline]
+    pub fn get(&self, row: usize, col: usize) -> Option<Token> {
+        let flat_i = self.to_flat_index(row, col).unwrap();
+        self.matrix.get(flat_i).copied()
+    }
+
+    /// Get a slice of the specified row of the matrix given the row index.
+    /// Panics if the row index is out of bounds of the matrix.
+    #[inline]
+    pub fn get_row(&self, row: usize) -> &[Token] {
+        let size = self.len();
+        let row_begin = self.to_flat_index(row, 0).unwrap();
+        let row_end = row_begin + size;
+        self.matrix
+            .get(row_begin..row_end)
+            .expect("Should not be out of bounds.")
+    }
+
+    /// Get the length/size of the matrix.
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.size
+    }
+
+    /// Given a row and column index and the size of a square matrix, return the index of the element in the flattened matrix.
+    /// Will panic if the row or column index is out of bounds of the matrix.
+    #[inline]
+    fn to_flat_index(&self, row: usize, col: usize) -> Result<usize, &'static str> {
+        let size = self.len();
+        match (row >= size, col >= size) {
+            (true, true) => Err("Row and column out of bounds."),
+            (true, false) => Err("Row out of bounds."),
+            (false, true) => Err("Column out of bounds."),
+            (false, false) => Ok(row * size + col),
+        }
+    }
+
+    /// Checks if the token matrix is symmetric.
+    #[inline]
+    pub fn is_symmetric(&self) -> bool {
+        let size = self.len();
+        for row_i in 0..size {
+            for col_i in 0..row_i {
+                if self.get(row_i, col_i) != self.get(col_i, row_i) {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
+#[inline]
 pub fn matrix_is_symmetric<T: PartialEq>(matrix: &[Vec<T>]) -> bool {
     let size = matrix.len();
     if let Some(first_row) = matrix.get(0) {
--- symmetric-word-triples/src/parser/token.rs
+++ symmetric-word-triples/src/parser/token.rs
@@ -1,16 +1,47 @@
 use dashmap::DashMap;
+use radix_trie::TrieKey;
 
 use super::wordfilter::Hr;
 
-pub struct Tkns {
-    tkn_to_string: DashMap<Tkn, String, Hr>,
+pub type TknSize = u16;
 
+#[derive(Hash, Clone, Copy, Debug, PartialEq, Eq)]
+pub struct Token(pub TknSize);
+
+#[derive(Clone, Debug)]
+pub struct Tokens {
+    encode: DashMap<String, Token, Hr>,
+    decode: DashMap<Token, String, Hr>,
+    size: TknSize,
 }
 
-pub struct Tkn(u16);
+impl Tokens {
+    /// Create a new `Tkns` instance. Empty with no tokens.
+    pub fn new() -> Tokens {
+        Tokens {
+            encode: DashMap::default(),
+            decode: DashMap::default(),
+            size: 0,
+        }
+    }
 
-impl Tkn {
-    fn to_string(&self) -> String {
-        todo!()
+    /// Insert a String into the `Tkns` instance.
+    /// Generates a new `Tkn` for the String and returns it.
+    pub fn insert(&self, s: String) {
+        let new_tkn = Token(self.len());
+        self.encode.insert(s.clone(), new_tkn);
+        self.decode.insert(new_tkn, s);
+    }
+
+    /// Returns the number of tokens in the `Tkns` instance.
+    pub fn len(&self) -> TknSize {
+        self.size
+    }
+}
+
+impl TrieKey for Token {
+    #[inline]
+    fn encode_bytes(&self) -> Vec<u8> {
+        self.0.encode_bytes()
     }
 }
